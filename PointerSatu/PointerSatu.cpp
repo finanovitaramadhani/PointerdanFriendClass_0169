387703961908db2e9af84b3c064ef8fc3bd3345d
finanovita <finanovita479@gmail.com>
2024-05-07 11:34:14 +0700

    Membuat class mahasiswa bersifat public

--- PointerSatu/PointerSatu.cpp
+++ PointerSatu/PointerSatu.cpp
@@ -4,11 +4,19 @@
 #include <iostream>
 using namespace std;
 
+class mahasiswa {
+public:
+	int nim;
+	void showNim() {
+		cout << " No Induk = " << nim << endl;
+
+	}
+};
 
 
 int main()
 {
-    
+
 }
 
 
